b52ba77be8d1f90fd73de49c428be6b2a0fd10bd
PeganovAnton <peganoff2@mail.ru>
2022-01-01 11:43:47 +0300

    Minor change in script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/test_seq_len_step_margin_commands/wiki_wmt_mbart_large.sh
+++ examples/nlp/token_classification/test_seq_len_step_margin_commands/wiki_wmt_mbart_large.sh
@@ -3,7 +3,7 @@ python test_seq_len_step_margin.py \
   --labels ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/labels_iwslt_en_text.txt \
   --source_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
   --output_dir ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/evelina_mbart_large_50_wiki_wmt_3_128 \
-  --max_seq_length 128 \
-  --margin 16 \
-  --step 8 \
+  --max_seq_length 128 130 \
+  --margin 16 18 \
+  --step 8 10 \
   --cuda_device 0
