10d2babdfff9021ff68595d93372d9744e8d55bd
PeganovAnton <peganoff2@mail.ru>
2022-01-03 16:44:03 +0300

    remove old results
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/tts/score_tts_on_iwslt.sh
+++ examples/tts/score_tts_on_iwslt.sh
@@ -3,6 +3,11 @@ spectrogram_generators=(tts_en_fastpitch tts_en_tacotron2 tts_en_glowtts)
 vocoders=(tts_waveglow_88m tts_squeezewave tts_uniglow tts_melgan tts_hifigan)
 
 output_dir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/test_tts_2_stages
+audio_dir="${output_dir}/audio"
+
+wer_file="${output_dir}/wer.txt"
+rm -r "${audio_dir}"/* "${output_dir}"/asr_preds__* "${output_dir}/asr_references.txt" "${wer_file}"
+rm -r
 
 for spectrogram_generator in "${spectrogram_generators[@]}"; do
   for vocoder in "${vocoders[@]}"; do
@@ -12,8 +17,8 @@ for spectrogram_generator in "${spectrogram_generators[@]}"; do
       --input ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/iwslt_en_text.txt \
       --asr_preds "${output_dir}/asr_preds__${spectrogram_generator}__${vocoder}.txt" \
       --asr_references "${output_dir}/asr_references.txt" \
-      --audio_preds "${output_dir}/audio/${spectrogram_generator}__${vocoder}" \
-      --wer_file "${output_dir}/wer.txt"
+      --audio_preds "${audio_dir}/${spectrogram_generator}__${vocoder}" \
+      --wer_file "${wer_file}"
   done
 done
 set +e +x
