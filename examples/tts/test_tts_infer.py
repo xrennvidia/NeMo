79dbb4d02f4196ad4bcf4f396b1c0826d218b5cc
PeganovAnton <peganoff2@mail.ru>
2022-01-05 10:57:45 +0300

    fix: apply vocoder correctly
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/tts/test_tts_infer.py
+++ examples/tts/test_tts_infer.py
@@ -145,16 +145,19 @@ def main():
         if torch.cuda.is_available():
             tts_input = tts_input.cuda()
         specs = tts_model_spec.generate_spectrogram(tokens=tts_input)
+        audio = []
+        step = ceil(len(specs) / 4)
+        for i in range(4):
+            audio.append(tts_model_vocoder.convert_spectrogram_to_audio(spec=specs[i * step: i * step + step]))
+        audio = [item for sublist in audio for item in sublist]
     else:
-        specs = []
+        audio = []
         for i, inp in enumerate(tts_input):
-            specs.append(tts_model_spec.generate_spectrogram(tokens=torch.tensor(inp).unsqueeze(0).cuda()))
-    audio = []
-    step = ceil(len(specs) / 4)
-    for i in range(4):
-        audio.append(tts_model_vocoder.convert_spectrogram_to_audio(spec=specs[i * step : i * step + step]))
-
-    audio = [item for sublist in audio for item in sublist]
+            audio.extend(
+                tts_model_vocoder.convert_spectrogram_to_audio(
+                    tts_model_spec.generate_spectrogram(tokens=torch.tensor(inp).unsqueeze(0).cuda())
+                )
+            )
     audio_file_paths = []
     # Save audio
     logging.debug(f"args.trim: {args.trim}")
