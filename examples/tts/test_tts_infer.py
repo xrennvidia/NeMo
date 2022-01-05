ad6865796206e29f2c400490df61793b1929d373
PeganovAnton <peganoff2@mail.ru>
2022-01-05 14:44:12 +0300

    Fix method call
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/tts/test_tts_infer.py
+++ examples/tts/test_tts_infer.py
@@ -184,7 +184,7 @@ def main():
             total_time = 0
             for inp in tts_input:
                 start = time()
-                audio.append(e2e_model.convert_text_to_waveform(inp))
+                audio.append(e2e_model.convert_text_to_waveform(tokens=inp))
                 total_time += time() - start
             average_time = total_time / len(tts_input)
         else:
