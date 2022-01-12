63fe89f5285d8605c456ed139d5f08f8ec9ca621
PeganovAnton <peganoff2@mail.ru>
2022-01-12 19:43:17 +0300

    fix: activate mwer env
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/commands/mwer_split_en_text.sh
+++ examples/nlp/machine_translation/commands/mwer_split_en_text.sh
@@ -3,6 +3,20 @@ mwer_xml=/home/apeganov/iwslt_2019_test_result/AAYNLarge6x6_all_punc_no_u_kenlm/
 mwer_txt=/home/apeganov/iwslt_2019_test_result/AAYNLarge6x6_all_punc_no_u_kenlm/punc_transcripts_not_segmented_input/stt_en_citrinet_1024_mwer_segmented.txt
 old_dir="$(pwd)"
 cd ~/mwerSegmenter/
+(
+  __conda_setup="$("/home/${USER}/anaconda3/bin/conda" 'shell.bash' 'hook' 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+  else
+    if [ -f "/home/${USER}/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/${USER}/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/${USER}/anaconda3/bin:$PATH"
+    fi
+  fi
+  unset __conda_setup
+  set -e
+  conda activate mwerSegmenter  # python 2 conda environment
 ./segmentBasedOnMWER.sh \
   "${dataset_dir}/IWSLT.TED.tst2019.en-de.de.xml" \
   "${dataset_dir}/IWSLT.TED.tst2019.en-de.en.xml" \
@@ -12,6 +26,8 @@ cd ~/mwerSegmenter/
   /home/apeganov/iwslt_2019_test_result/AAYNLarge6x6_all_punc_no_u_kenlm/punc_transcripts_not_segmented_input/stt_en_citrinet_1024_mwer_segmented.xml \
   no \
   1
+  conda deactivate
+)
 cd old_dir
 python ~/NeMo/examples/speech_translation/test_iwslt_and_perform_all_ops_common_scripts/xml_2_text_segs_2_lines.py \
   -i "${mwer_xml}" \
