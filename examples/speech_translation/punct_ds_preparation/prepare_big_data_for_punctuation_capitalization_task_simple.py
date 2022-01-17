7de176aee7c7358abd5eea2f5ec3a5cd16a0f419
PeganovAnton <peganoff2@mail.ru>
2022-01-17 11:02:28 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/prepare_big_data_for_punctuation_capitalization_task_simple.py
+++ examples/speech_translation/punct_ds_preparation/prepare_big_data_for_punctuation_capitalization_task_simple.py
@@ -887,14 +887,14 @@ def preprocess_news_crawl(
         tmp_files, source_files, start_lines, end_lines = split_large_files_into_small_files(
             dir_path, Path(tmp_dir), NUM_LINES_PER_NEWS_CRAWL_TMP_FILE
         )
-    doc_ids = list(range(start_file_id, start_doc_id + len(tmp_files)))
-    file_ids = list(range(start_file_id, start_file_id + len(tmp_files)))
-    with Progress(len(tmp_files), "Preparing extracted Wikipedia", "doc") as progress_queues:
-        with mp.Pool(num_jobs) as pool:
-            pool.starmap(
-                NewsCrawlWorker(document_dir, lang, tokenizer, progress_queues[0]),
-                zip(tmp_files, file_ids, doc_ids, source_files, start_lines, end_lines, range(len(tmp_files))),
-            )
+        doc_ids = list(range(start_file_id, start_doc_id + len(tmp_files)))
+        file_ids = list(range(start_file_id, start_file_id + len(tmp_files)))
+        with Progress(len(tmp_files), "Preparing extracted Wikipedia", "doc") as progress_queues:
+            with mp.Pool(num_jobs) as pool:
+                pool.starmap(
+                    NewsCrawlWorker(document_dir, lang, tokenizer, progress_queues[0]),
+                    zip(tmp_files, file_ids, doc_ids, source_files, start_lines, end_lines, range(len(tmp_files))),
+                )
 
 
 def is_int(s):
