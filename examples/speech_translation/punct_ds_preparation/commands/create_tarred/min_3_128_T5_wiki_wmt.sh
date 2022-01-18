1b28080b36b236ebebe435d612aee296d38335a8
PeganovAnton <peganoff2@mail.ru>
2022-01-18 08:24:16 +0300

    Fix minor bug
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/commands/create_tarred/min_3_128_T5_wiki_wmt.sh
+++ examples/speech_translation/punct_ds_preparation/commands/create_tarred/min_3_128_T5_wiki_wmt.sh
@@ -5,7 +5,7 @@ python ~/NeMo/examples/nlp/token_classification/data/create_punctuation_capitali
   --labels /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_17.01.2022/train/bert_labels.txt \
   --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_17.01.2022/for_upload/train_t5_tarred_${tokens_in_batch} \
   --tokens_in_batch="${tokens_in_batch}" \
-  --num_batches_per_tar_file 100 \
+  --num_batches_per_tarfile 100 \
   --lines_per_dataset_fragment 200000 \
   --tokenizer_name t5-large \
   --use_fast_tokenizer \
