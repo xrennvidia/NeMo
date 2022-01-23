8e02d1fd2b7fd3e24b686aaf301e02f16dae1d5f
PeganovAnton <peganoff2@mail.ru>
2022-01-23 19:15:59 +0300

    Make improvements in scripts
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/ngc_commands/command_train1_on_wiki_wmt_intact_sentences.sh
+++ examples/nlp/token_classification/ngc_commands/command_train1_on_wiki_wmt_intact_sentences.sh
@@ -4,7 +4,8 @@ exp_name="$2"
 gpus="$3"
 
 set -e -x
-mkdir -p /result/nemo_experiments
+result_dir=/result/nemo_experiments
+mkdir -p "${result_dir}"
 cd /workspace/NeMo
 git checkout feat/punc_tarred
 git pull
@@ -14,6 +15,7 @@ wandb login ${WANDB_API_KEY}
 python punctuation_capitalization_train_evaluate.py --config-path=conf/wiki_wmt \
     --config-name local_base_intact_sentences_bs20k_lr1e-4_steps500k_from_pretrained \
     exp_manager.wandb_logger_kwargs.name=${exp_name} \
+    +exp_manager.explicit_log_dir="${result_dir}" \
     trainer.gpus=${gpus} \
     model.train_ds.ds_item=/data/train_bert_tarred_10000 \
     model.validation_ds.ds_item=[/data/IWSLT_tst2019,/data/europarl_dev,/data/news_commentary_dev,/data/news_crawl_dev,/data/rapid_dev]
--- examples/nlp/token_classification/ngc_commands/train1_on_wiki_wmt_from_scratch_intact_sentences.sh
+++ examples/nlp/token_classification/ngc_commands/train1_on_wiki_wmt_from_scratch_intact_sentences.sh
@@ -5,7 +5,8 @@ gpus=8
 
 read -r -d '' command << EOF
 set -e -x
-mkdir -p /result/nemo_experiments
+result_dir=/result/nemo_experiments
+mkdir -p "${result_dir}"
 cd NeMo
 git checkout feat/punc_tarred
 git pull
@@ -16,6 +17,7 @@ python -c "from transformers import AutoTokenizer; tokenizer = AutoTokenizer.fro
 python punctuation_capitalization_train_evaluate.py --config-path=conf/wiki_wmt \
     --config-name local_base_intact_sentences_bs20k_lr1e-4_steps500k \
     exp_manager.wandb_logger_kwargs.name=${exp_name} \
+    +exp_manager.explicit_log_dir="${result_dir}" \
     trainer.gpus=${gpus} \
     model.train_ds.ds_item=/data/train_bert_tarred_10000 \
     model.validation_ds.ds_item=[/data/IWSLT_tst2019,/data/europarl_dev,/data/news_commentary_dev,/data/news_crawl_dev,/data/rapid_dev]
