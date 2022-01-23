b122d18f75af490fb2a4bd7ac4adfe8aa9229335
PeganovAnton <peganoff2@mail.ru>
2022-01-23 10:11:59 +0300

    tarred dataset creation command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/commands/create_tarred/autoregressive_wprefix_all_punc_no_u_3_128_nemo_wiki_wmt.sh
@@ -0,0 +1,21 @@
+tokens_in_batch="$1"
+num_batches_per_tarfile=50
+lines_per_dataset_fragment=200000
+
+work_dir=/media/apeganov/DATA/punctuation_and_capitalization/all_punc_no_u/3_128/wiki_wmt_18.01.2022
+
+
+python ~/NeMo/examples/nlp/machine_translation/create_tarred_parallel_dataset.py \
+    --src_fname "${work_dir}/train/input.txt" \
+    --tgt_fname "${work_dir}/train/autoregressive_labels_wprefix.txt" \
+    --out_dir "${work_dir}/for_upload/train_autoregressive_wprefix_tarred_${tokens_in_batch}" \
+    --encoder_tokenizer_model "${work_dir}/for_upload/input.BPE.25000.model" \
+    --encoder_tokenizer_bpe_dropout 0 \
+    --encoder_tokenizer_coverage 1.0 \
+    --decoder_tokenizer_model "${work_dir}/for_upload/autoregressive_labels_wprefix.BPE.18.model" \
+    --decoder_tokenizer_bpe_dropout 0 \
+    --decoder_tokenizer_coverage 1.0 \
+    --tokens_in_batch "${tokens_in_batch}" \
+    --num_batches_per_tarfile "${num_batches_per_tarfile}" \
+    --lines_per_dataset_fragment "${lines_per_dataset_fragment}" \
+    --clean
