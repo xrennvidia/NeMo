73774033ce16b3862620102af6d295c03b1e5447
PeganovAnton <peganoff2@mail.ru>
2022-01-26 17:52:35 +0300

    Update script for creating tarred dataset
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/commands/create_tarred_dataset_for_asr_punc_cap_augmentation.sh
+++ examples/nlp/machine_translation/commands/create_tarred_dataset_for_asr_punc_cap_augmentation.sh
@@ -6,7 +6,7 @@ lines_per_dataset_fragment=200000
 python ~/NeMo/examples/nlp/machine_translation/create_tarred_parallel_dataset.py \
     --src_fname "/home/apeganov/en_de_shuffled/augmentation_results/augmented.normalized.en" \
     --tgt_fname "/home/apeganov/en_de_shuffled/train.shuffled.de" \
-    --out_dir "/media/apeganov/DATA/ASR_PC_augmentation_of_en_WMT_src" \
+    --out_dir "/media/apeganov/DATA/ASR_PC_augmentation_of_en_WMT_src/for_upload/train_tarred_${tokens_in_batch}" \
     --encoder_tokenizer_model "/home/apeganov/checkpoints/wmt21_en_de_backtranslated_24x6_averaged_finetuned_13.1.21/071dff1d034e466f935ef6349ede7648_shared_tokenizer.32000.BPE.model" \
     --encoder_tokenizer_bpe_dropout 0 \
     --encoder_tokenizer_coverage 1.0 \
