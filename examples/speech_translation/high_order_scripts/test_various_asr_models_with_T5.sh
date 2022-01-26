7c3ad5ab408c3e6d7425bc42548ca821893417f7
PeganovAnton <peganoff2@mail.ru>
2022-01-27 00:36:54 +0300

    Add script for varying ASR models
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/high_order_scripts/test_various_asr_models_with_T5.sh
@@ -0,0 +1,37 @@
+asr_models=(
+  stt_en_citrinet_1024
+  ~/checkpoints/CitriNet-1024-8x-Stride-Gamma-0.25.nemo
+  ~/checkpoints/Citrinet-1024-SPE-Unigram-1024-Jarvis-ASRSet-2.0_no_weight_decay_e250-averaged.nemo
+  QuartzNet15x5Base-En
+  stt_en_citrinet_256
+  stt_en_citrinet_512
+  stt_en_jasper10x5dr
+)
+
+for asr_model in "${asr_models[@]}"; do
+  bash test_iwslt.sh \
+    ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019 \
+    "${asr_model}" \
+    ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_t5_large_lr5e-6_steps300000/checkpoints/Punctuation_and_Capitalization.nemo \
+    ~/checkpoints/wmt21_en_de_backtranslated_24x6_averaged.nemo \
+    "~/iwslt_2019_test_result/varying_asr_for_evelina_T5_min_punc_kenlm/${asr_model}" \
+    0 \
+    1 \
+    0 \
+    1 \
+    0 \
+    1 \
+    /media/apeganov/DATA/punctuation_and_capitalization/simplest/92_128/wmt_92_128_14.12.2021/train/ngram6.arpa
+  bash test_iwslt.sh \
+    ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019 \
+    "${asr_model}" \
+    ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_t5_large_lr5e-6_steps300000/checkpoints/Punctuation_and_Capitalization.nemo \
+    ~/checkpoints/wmt21_en_de_backtranslated_24x6_averaged.nemo \
+    "~/iwslt_2019_test_result/varying_asr_for_evelina_T5_min_punc/${asr_model}" \
+    0 \
+    1 \
+    0 \
+    1 \
+    0 \
+    1
+done
