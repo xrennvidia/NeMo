760e4257ae0e6c0ad648c51be1b4b7e1c294564a
PeganovAnton <peganoff2@mail.ru>
2022-01-26 14:22:57 +0300

    Reduce overlapppings
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/commands/punctuate_capitalize_embedding_tips_nmt_iwslt_trained_on_wiki_wmt_news_crawl_large6x6_6_overlapping.sh
+++ examples/nlp/machine_translation/punctuation_infer/commands/punctuate_capitalize_embedding_tips_nmt_iwslt_trained_on_wiki_wmt_news_crawl_large6x6_6_overlapping.sh
@@ -1,14 +1,14 @@
 set -e
 work_dir="~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model2"
 output_dir="${work_dir}/nmt_tips"
-pred_labels="${output_dir}/embedding_tips_large6x6_15_overlapping_labels.txt"
+pred_labels="${output_dir}/embedding_tips_large6x6_6_overlapping_labels.txt"
 python punctuate_capitalize_nmt.py \
     --input_text "${work_dir}/text_iwslt_en_text.txt" \
-    --output_text "${output_dir}/embedding_tips_large6x6_15_overlapping_text.txt" \
+    --output_text "${output_dir}/embedding_tips_large6x6_6_overlapping_text.txt" \
     --output_labels "${pred_labels}" \
     --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/tips_all_punc_no_u_nmt_wiki_wmt_news_crawl_large6x6_bs400000_steps400000_lr2e-4/checkpoints/AAYNLarge6x6.nemo \
     --max_seq_length 128 \
-    --step 4 \
+    --step 16 \
     --margin 16 \
     --batch_size 80 \
     --no_all_upper_label \
@@ -18,7 +18,7 @@ python punctuate_capitalize_nmt.py \
 python compute_metrics.py \
     --hyp ${pred_labels} \
     --ref "${work_dir}/labels_iwslt_en_text.txt" \
-    --output "${output_dir}/embedding_tips_large6x6_15_overlapping_scores.json" \
+    --output "${output_dir}/embedding_tips_large6x6_6_overlapping_scores.json" \
     --normalize_punctuation_in_hyp \
     --reference_evelina_data_format
 set +e
