17de06df587d1e019255b16d359dc6ac674f102d
PeganovAnton <peganoff2@mail.ru>
2022-01-27 18:04:50 +0300

    Add script for step effect demonstration
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/commands/demonstrate_step_effect.sh
+++ examples/nlp/machine_translation/punctuation_infer/commands/demonstrate_step_effect.sh
@@ -1,28 +1,22 @@
 set -e
 model=all_punc_no_u_nmt_wiki_wmt_news_crawl_large6x6_bs400000_steps400000_lr2e-4
 work_dir=~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model2
-output_dir="${work_dir}/nmt/demonstrate/${model}/margin"
+output_dir="${work_dir}/nmt/demonstrate/${model}/step"
 mkdir -p "${output_dir}"
 max_seq_length=128
-margins=( 0 1 2 4 8 12 16 24 36 48 )
-declare -a steps
-for margin in "${margins[@]}"; do
-  steps+=( $(expr ${max_seq_length} - 2 \* ${margin}) )
-done
+steps=( 4 8 16 32 64 128 )
 
-for i in "${!margins[@]}"; do
-  margin="${margins["${i}"]}"
-  step="${steps["${i}"]}"
-  echo "margin=${margin} step=${step}"
-  pred_labels="${output_dir}/labels${margin}.txt"
+for step in "${steps[@]}"; do
+  echo "step=${step}"
+  pred_labels="${output_dir}/labels${step}.txt"
   python punctuate_capitalize_nmt.py \
       --input_text "${work_dir}/text_iwslt_en_text.txt" \
-      --output_text "${output_dir}/text${margin}.txt" \
+      --output_text "${output_dir}/text${step}.txt" \
       --output_labels "${pred_labels}" \
       --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/"${model}"/checkpoints/AAYNLarge6x6.nemo \
       --max_seq_length "${max_seq_length}" \
       --step "${step}" \
-      --margin "${margin}" \
+      --margin 0 \
       --batch_size 80 \
       --no_all_upper_label \
       --add_source_num_words_to_batch \
@@ -31,6 +25,6 @@ for i in "${!margins[@]}"; do
   python compute_metrics.py \
       --hyp ${pred_labels} \
       --ref "${work_dir}/all_punc_no_u/autoregressive_labels.txt" \
-      --output "${output_dir}/scores${margin}.json"
+      --output "${output_dir}/scores${step}.json"
 done
 set +e
