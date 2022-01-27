3bd3610de5a3ce89d2022f1bd67639ea058ae53a
PeganovAnton <peganoff2@mail.ru>
2022-01-27 17:08:54 +0300

    Change dataset
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/machine_translation/punctuation_infer/commands/score_dev19_without_beam_search_length_adjustment.sh
+++ examples/nlp/machine_translation/punctuation_infer/commands/score_dev19_without_beam_search_length_adjustment.sh
@@ -1,7 +1,7 @@
-work_dir=/media/apeganov/DATA/punctuation_and_capitalization/all_punc_no_u/3_128/wiki_wmt_18.01.2022
+work_dir=/media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_17.01.2022
 output_dir="${work_dir}/inference_on_IWSLT_tst2019_results"
 model_name=nmt_wmt_base_bs800000_steps300000_lr2e-4
 python compute_metrics.py \
   --hyp "${output_dir}/${model_name}_without_adjustment_labels.txt" \
-  --ref "${work_dir}/for_upload/IWSLT_tst2019/autoregressive_labels.txt" \
+  --ref "${work_dir}/IWSLT_tst2019/autoregressive_labels.txt" \
   --output "${output_dir}/${model_name}_without_adjustment_scores.json"
