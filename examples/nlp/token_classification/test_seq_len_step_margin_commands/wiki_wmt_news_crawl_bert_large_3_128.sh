a11f1c93a7d613b99b53ec596c65ba6d65dfae42
PeganovAnton <peganoff2@mail.ru>
2022-01-28 12:22:55 +0300

    Update script
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/nlp/token_classification/test_seq_len_step_margin_commands/wiki_wmt_news_crawl_bert_large_3_128.sh
+++ examples/nlp/token_classification/test_seq_len_step_margin_commands/wiki_wmt_news_crawl_bert_large_3_128.sh
@@ -1,8 +1,8 @@
 python test_seq_len_step_margin.py \
-  --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_3_128_large_lr6e-5_steps300k/checkpoints/Punctuation_and_Capitalization.nemo \
+  --model_path ~/NWInf_results/autoregressive_punctuation_capitalization/evelina_wiki_wmt_news_crawl_3_128_large_lr1e-4_bs640k_steps300k/checkpoints/Punctuation_and_Capitalization.nemo \
   --labels ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/labels_iwslt_en_text.txt \
   --source_text ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/text_iwslt_en_text.txt \
-  --output_dir ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/evelina_model_large_wiki_wmt_3_128 \
+  --output_dir ~/data/iwslt/IWSLT-SLT/eval/en-de/IWSLT.tst2019/for_testing_punctuation_model/evelina_model_bert_large_wiki_wmt_news_crawl_3_128 \
   --max_seq_length 128 \
   --margin 16 \
   --step 8 \
