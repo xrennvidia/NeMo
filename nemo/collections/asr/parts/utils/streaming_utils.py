fc54062e78a11271a145ffac7858fb3ef4194a4f
fayejf <fayejf07@gmail.com>
2022-03-01 12:22:38 -0800

    -1
    Signed-off-by: fayejf <fayejf07@gmail.com>

--- nemo/collections/asr/parts/utils/streaming_utils.py
+++ nemo/collections/asr/parts/utils/streaming_utils.py
@@ -112,7 +112,7 @@ class AudioFeatureIterator(IterableDataset):
         if last <= self._features_len[0]:
             frame = self._features[:, self._start : last].cpu()
             self._start = last
-            if self._start == self._features_len[0]:
+            if self._start == self._features_len[0]-1:
                 self.output = False
 
         else:
