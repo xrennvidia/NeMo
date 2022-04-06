fd879a82789d4ae3fc8f2810bb8d8f843442ed6b
PeganovAnton <peganoff2@mail.ru>
2022-04-06 11:38:21 +0300

    Add new command for wiki extracted preparation
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/commands/simplest/3_128/wiki_extracted_wmt2020.sh
@@ -0,0 +1,15 @@
+python prepare_big_data_for_punctuation_capitalization_task_simple.py \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_extracted_wmt2020_x2_06.04.2022 \
+  --corpus_types wiki-extracted \
+  --create_model_input \
+  --bert_labels \
+  --autoregressive_labels \
+  --sequence_length_range 3 128 \
+  --allowed_punctuation '.,?' \
+  --only_first_punctuation_character_after_word_in_autoregressive \
+  --no_label_if_all_characters_are_upper_case \
+  --input_files_or_dirs /media/apeganov/DATA/wikipedia.en.lid_filtered.test_filtered_split \
+  --num_jobs 24 \
+  --num_passes_through_dataset 2 \
+  --dev_size 10000 \
+  --test_size 0
