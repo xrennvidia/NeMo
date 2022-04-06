3559201436c384a4111cc7d7d701df0de8b445db
PeganovAnton <peganoff2@mail.ru>
2022-04-06 23:40:55 +0300

    Update tarred command
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/speech_translation/punct_ds_preparation/commands/create_tarred/evelina_min_3_128_T5_wiki_wmt.sh
+++ examples/speech_translation/punct_ds_preparation/commands/create_tarred/evelina_min_3_128_T5_wiki_wmt.sh
@@ -1,9 +1,9 @@
 tokens_in_batch="$1"
 
 python ~/NeMo/examples/nlp/token_classification/data/create_punctuation_capitalization_tarred_dataset.py \
-  --text /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_17.01.2022/train/input.txt \
-  --labels /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_17.01.2022/train/bert_labels.txt \
-  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_17.01.2022/for_upload/train_t5_tarred_${tokens_in_batch} \
+  --text /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_06.04.2022/train/input.txt \
+  --labels /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_06.04.2022/train/bert_labels.txt \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/simplest/3_128/wiki_wmt_06.04.2022/for_upload/train_t5_tarred_${tokens_in_batch} \
   --tokens_in_batch="${tokens_in_batch}" \
   --num_batches_per_tarfile 100 \
   --lines_per_dataset_fragment 200000 \
