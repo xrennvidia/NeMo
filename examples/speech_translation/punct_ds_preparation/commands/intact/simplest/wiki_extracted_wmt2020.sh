26bd29476002c71a1530e3265cc59ac37eead867
PeganovAnton <peganoff2@mail.ru>
2022-04-06 18:20:05 +0300

    Add command for intact sentences
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

---
+++ examples/speech_translation/punct_ds_preparation/commands/intact/simplest/wiki_extracted_wmt2020.sh
@@ -0,0 +1,14 @@
+python prepare_big_data_for_punctuation_capitalization_task_simple.py \
+  --output_dir /media/apeganov/DATA/punctuation_and_capitalization/intact/simplest/wiki_extracted_wmt2020_x2_06.04.2022 \
+  --corpus_types news-crawl \
+  --create_model_input \
+  --bert_labels \
+  --autoregressive_labels \
+  --allowed_punctuation '.,?' \
+  --only_first_punctuation_character_after_word_in_autoregressive \
+  --no_label_if_all_characters_are_upper_case \
+  --input_files_or_dirs /media/apeganov/DATA/wikipedia.en.lid_filtered.test_filtered_dir \
+  --num_jobs 24 \
+  --dev_size 10000 \
+  --test_size 0 \
+  --intact_sentences
