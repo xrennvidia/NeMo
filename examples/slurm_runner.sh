1483b3dedca581d03c8872810eec09cc5cc7fb98
PeganovAnton <peganoff2@mail.ru>
2022-04-07 12:09:33 +0300

    Improve slurm runner
    Signed-off-by: PeganovAnton <peganoff2@mail.ru>

--- examples/slurm_runner.sh
+++ examples/slurm_runner.sh
@@ -1,7 +1,8 @@
 set -e -x
 
-sbatch_command="$1"
+slurm_script="$1"
 results_dir="$2"
+model_name="$3"
 
 output=$(eval ${sbatch_command})
 job_id=$(echo ${output} | grep -oP "\d+")
@@ -17,7 +18,7 @@ path_to_errors="$(find "${results_dir}" -name "error-${job_id}-0.out")"
 
 while [ ! -z "$(grep 'DUE TO TIME LIMIT ***' "${path_to_errors}")" ]; do
   echo RESTARTING
-  output=$(eval ${sbatch_command})
+  output=$(eval "sbatch --nv-meta ml-model.${model_name} ${slurm_script}")
   job_id=$(echo ${output} | grep -oP "\d+")
   echo "Current job id: ${job_id}"
 
